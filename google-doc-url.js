d99d0befa26e1e757d9301a080d778f3c6e7ca75
VillaServices <79458993+VillaServices@users.noreply.github.com>
2021-03-10 14:40:15 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KZr2y_RwUtFp925hb0gbWTOeT0A_VUo6oc8GluVHcdI/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBQtL1Wa96SRPUlJo7KviuJyjA53jpBoTk';
+// var googleApiKey = 'AIzaSyBQtL1Wa96SRPUlJo7KviuJyjA53jpBoTk';
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
